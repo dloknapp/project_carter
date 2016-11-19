944469193c37607a82365f1d8d556f9fb8666261
Raghu Betina <raghu@firstdraft.com>
2016-11-19 20:21:47 +0000

    Whitelist columns in Vote for ActiveAdmin

--- app/admin/vote.rb
+++ app/admin/vote.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Vote do
 
+ permit_params :yes_vote, :no_vote, :user_id, :image_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
